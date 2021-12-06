888b6183addccb1e4764117eee11857fa7f64040
bellsmith15 <smith27n@gmail.com>
2021-12-06 23:59:03 +0000

    Add java rules button

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -3,6 +3,9 @@
 const playerChoices = document.getElementsByClassName('choice');
 const roundText = document.getElementById('round-text');
 const closeBtn = document.getElementsByClassName('close')[0];
+
+const openBtn = document.getElementsByClassName('open')[0];
+
 const winnerModal = document.getElementsByClassName('winner-modal')[0];
 const modalHeader = document.getElementsByClassName('modal-header')[0];
 const modalParagraph = document.getElementsByClassName('modal-result')[0];
@@ -111,11 +114,11 @@ function incrementComputerScore(score) {
 
 
 function showWinner(winnerModal, modalHeader, modalParagraph) {
-    if (playerScore === 9) {
+    if (playerScore === 3) {
         winnerModal.style.display = 'block';
         modalHeader.textContent = 'You Win The Game!';
         modalParagraph.textContent = `You: ${playerScore} > Computer: ${computerScore}`;
-    } else if (computerScore === 9) {
+    } else if (computerScore === 3) {
         winnerModal.style.display = 'block';
         modalHeader.textContent = 'You Lose The Game!';
         modalParagraph.textContent = `Computer: ${computerScore} > You: ${playerScore}`;
@@ -134,5 +137,37 @@ function resetGame(player, computer) {
     computer.innerHTML = computerScore;
     roundText.innerHTML = '';
     document.getElementById('computer-icon').className = '';
+}
+
+
+
+// function showRules (rulesModal) {
+//     rulesModal.style.display = 'block';
+// }
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("myBtn");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
 }
 
