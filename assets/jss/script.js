47c07d9ae509c1a3f8eae3d07466053564632c9f
bellsmith15 <smith27n@gmail.com>
2021-12-11 05:08:51 +0000

    Media responsiveness Desktop

--- assets/jss/script.js
+++ assets/jss/script.js
@@ -18,6 +18,35 @@ let computerScore = 0;
 
 const musicSound = new Audio("assets/music/christmas-magic-night-11648.mp3");
 
+
+// https://www.w3schools.com/howto/howto_css_modals.asp
+
+let modal = document.getElementById("myModal"); /* Get modal*/
+let btn = document.getElementById("myBtn"); /* Get button that opens modal*/
+let span = document.getElementsByClassName("shut")[0]; /* Get the <span> element that closes the modal */
+
+// When the user clicks the button, open the modal 
+btn.onclick = function() {
+  modal.style.display = "block";
+};
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+};
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+};
+
+
+
+
+
+
 // function playMusic() {
 //     musicSound.play();
 // }
@@ -165,31 +194,4 @@ function resetGame(player, computer) {
 }
 
 
-// https://www.w3schools.com/howto/howto_css_modals.asp
-
-// Get the modal
-var modal = document.getElementById("myModal");
-
-// Get the button that opens the modal
-var btn = document.getElementById("myBtn");
-
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-};
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-};
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-};
 
